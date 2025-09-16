3d630d8c4222258e277d9ccfa9aec4e5cca4ce35
Jocselyn Aguilar <jocselynaguilar@Jocselyns-MacBook-Pro.local>
2025-09-15 19:43:21 -0600

    Fixing linter error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import SpaceMono from "../assets/fonts/SpaceMono-Regular.ttf";
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
@@ -24,7 +24,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono,
     ...FontAwesome.font,
   });
 
