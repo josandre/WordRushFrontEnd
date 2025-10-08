413b96aac7ff8751834cf4fb49ab228c4ef125d0
Jocselyn Aguilar <jocselynaguilar@Jocselyns-MacBook-Pro.local>
2025-10-07 21:11:58 -0600

    UI Support for web

--- app/screens/Introduction/Introduction.tsx
+++ app/screens/Introduction/Introduction.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, StatusBar, ScrollView } from 'react-native'
+import { Dimensions, StatusBar, ScrollView, Platform } from 'react-native'
 import React from 'react'
 import style from '../../theme/style';
 import { Colors } from '../../theme/color';
@@ -6,39 +6,44 @@ import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AppNavigation } from '@/app/navigator/AppNavigationTypes';
 import IntroSlide from '../../components/organisms/IntroSlide';
-import { FIRST_SLICE_TITLE, SECOND_SLICE_TITLE, INTRO_BG, INTRO_IMG_1, INTRO_IMG_2 } from './constants';
+import { INTRO_BG, FIRST_SLICE_TITLE, SECOND_SLICE_TITLE } from './constants';
 
-const width = Dimensions.get('screen').width
-const height = Dimensions.get('screen').height
 
 export default function Introduction() {
     const navigation = useNavigation<AppNavigation>();
 
     return (
         <SafeAreaView style={[style.area, { backgroundColor: Colors.bg, }]}>
+
             <StatusBar translucent={true} backgroundColor={'transparent'} barStyle={'light-content'} />
 
-            <ScrollView horizontal showsHorizontalScrollIndicator={false} pagingEnabled={true}>
-                <IntroSlide
-                    background={INTRO_BG}
-                    image={INTRO_IMG_1}
-                    dotsTotal={2}
-                    dotsActiveIndex={0}
-                    ctaTitle={FIRST_SLICE_TITLE}
-                    onSignUp={() => navigation.navigate('Option')}
-                    onLogin={() => navigation.navigate('Option')}
-                    imageHeightRatio={3}
-                />
-                <IntroSlide
-                    background={INTRO_BG}
-                    image={INTRO_IMG_2}
-                    dotsTotal={2}
-                    dotsActiveIndex={2}
-                    ctaTitle={SECOND_SLICE_TITLE}
-                    onSignUp={() => navigation.navigate('Option')}
-                    onLogin={() => navigation.navigate('Option')}
-                    imageHeightRatio={3}
-                />
+            <ScrollView 
+                horizontal 
+                showsHorizontalScrollIndicator={false} 
+                pagingEnabled={true}
+                style={Platform.OS === 'web' ? { pointerEvents: 'auto' } : {}}
+            >
+
+                    <IntroSlide
+                        background={INTRO_BG}
+                        dotsTotal={2}
+                        dotsActiveIndex={0}
+                        ctaTitle={FIRST_SLICE_TITLE}
+                        onSignUp={() => navigation.navigate('Option')}
+                        onLogin={() => navigation.navigate('Option')}
+                        imageHeightRatio={3}
+                    />
+                    <IntroSlide
+                        background={INTRO_BG}
+                        dotsTotal={2}
+                        dotsActiveIndex={1}
+                        ctaTitle={SECOND_SLICE_TITLE}
+                        onSignUp={() => navigation.navigate('Option')}
+                        onLogin={() => navigation.navigate('Option')}
+                        imageHeightRatio={3}
+                    />
+
+        
             </ScrollView>
         </SafeAreaView>
     )
--- app/screens/Splash.tsx
+++ app/screens/Splash.tsx
@@ -7,16 +7,15 @@ import bgImage from '@/assets/image/bg.png'
 import logo from '@/assets/image/Logo.png'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const width = Dimensions.get('screen').width
-const height = Dimensions.get('screen').height
 
 export default function Splash() {
     const navigation = useNavigation();
+    
     return (
         <SafeAreaView style={[style.area, { backgroundColor: Colors.bg,  }]}>
             <StatusBar translucent={true} backgroundColor='transparent' barStyle={'light-content'} />
             <ImageBackground source={bgImage} resizeMode='stretch' style={{ flex: 1,justifyContent:'center',alignItems:'center' }}>
-                <Image source={logo} resizeMode='stretch' style={{ height: 166, width: 140 }} />
+                <Image source={logo} resizeMode='stretch' style={{ height: 180, width: 180 }} />
             </ImageBackground>
         </SafeAreaView>
     )
